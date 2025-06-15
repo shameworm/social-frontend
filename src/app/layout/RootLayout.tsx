befd59a26fddbc51ea1ecd3a479605073f5004bf
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2025-06-15 19:46:22 +0300

    refactor(shared/logo): provide classname props to change logo styling

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -4,7 +4,7 @@ import { PageLayout } from '@/shared/ui/page-layout';
 import { Logo } from '@/shared/ui/logo';
 
 export function RootLayout() {
-  const topBar = <Logo />;
+  const topBar = <Logo className="my-10" />;
   return (
     <PageLayout topBar={topBar}>
       <Outlet />
--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -1,8 +1,13 @@
+import { cn } from '@/shared/lib/utils';
 import { Waypoints } from 'lucide-react';
 
-export function Logo() {
+type LogoProps = {
+  className?: string;
+};
+
+export function Logo({ className }: LogoProps) {
   return (
-    <div className="flex justify-center mt-16 mb-32">
+    <div className={cn('flex justify-center', className)}>
       <div className="flex items-center space-x-4">
         <Waypoints className="h-8 w-8 text-primary" />
         <h1 className="font-extrabold font-sans text-lg text-foreground ">Connect Social</h1>
